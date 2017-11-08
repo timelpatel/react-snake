1c3455d2ea3e0df2f9edb7ef07db4353c2d0df4b
Timel Patel <timel.patel@timelab.co.uk>
2017-11-08 13:58:46 +0000

    Initial commit. Setup local server and development env.

---
+++ src/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const App = () => (
+    <div>
+        <h1>Snake Game</h1>
+    </div>
+)
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+)
